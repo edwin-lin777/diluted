63c1f059626706a969feb4331f7bcbed7ccc9d29
James Weng <120286763+jweng121@users.noreply.github.com>
2025-11-04 21:10:51 -0500

    didy

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 const page = () => {
   return (
-    <div className="underline text-5xl ">Welcome to diluted</div>
+    <div>
+      <div className="underline text-5xl ">Welcome to diluted</div>
+      <div className='font-extrabold'>Welcome to diddy</div>
+    </div>
   )
 }
 
