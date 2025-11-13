98fe84a19337c2ce2463031028e5817b0337834c
James Weng <120286763+jweng121@users.noreply.github.com>
2025-11-12 22:03:16 -0500

    Updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,38 @@
 import Link from "next/link";
+import { Cal_Sans } from "next/font/google";
+
+const cal = Cal_Sans({
+  weight: '400',
+})
+
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100 shadow-sm flex  justify-left fixed top-0 z-100">
-      <ul className="flex gap-35 ml-15">
-        <li className = "btn btn-ghost text-xl">
-        Diluted
+      <div className="navbar flex shadow-xs justify-left fixed top-10 z-100">
+        <div className='flex-col flex'>
 
-        </li>
-        <li>
-          <Link href="/" className="btn btn-ghost text-xl">
-            Leaderboards
-          </Link>
-        </li>
-        <li>
-          <Link href="/discover" className="btn btn-ghost text-xl">
-            Discover
-          </Link>
-        </li>
-      </ul>
+          <ul className="translate-y-[-40px] flex ml-15">
+            <li className = "cal-sans-regular btn btn-ghost text-3xl">
+            Diluted
+
+            </li>
+              <li>
+                <Link href="/" className="btn btn-ghost roboto-flex-class font-medium">
+                  Leaderboards
+                </Link>
+              </li>
+              <li>
+                <Link href="/discover" className="btn btn-ghost roboto-flex-class font-medium">
+                  Discover
+                </Link>
+              </li>
+          </ul>     
+        <div className='fixed ml-21 py-3'>
+          <ul className='flex gap-7'>
+            <li className='text-gray-400 hover:text-black transition-colors'><Link href='' className=''>Trending</Link></li>
+            <li className='text-gray-400 hover:text-black transition-colors'><Link href='' className=''>For You</Link></li>
+          </ul>
+        </div> 
+      </div>
     </div>
   );
 }
